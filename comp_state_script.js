b56df1ae9add0ecc3883f1d43fcf8b35974711ea
Tobani <tobanigeorge@gmail.com>
2022-08-10 18:21:41 +0100

    added in the completed page

---
+++ comp_state_script.js
@@ -0,0 +1,4 @@
+document.querySelector('.card-cvc').innerHTML = sessionStorage.CARD_CVC;
+document.querySelector('.card-number-front').innerHTML = sessionStorage.CARD_NUM;
+document.querySelector('.card-holder-name-front').innerHTML = sessionStorage.NAME;
+document.querySelector('.card-exp-front').innerHTML = sessionStorage.CARD_EXP_DATE;
